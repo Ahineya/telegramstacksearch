0ee24a0c37d21320818447e269c4804e7df411ae
Pavel_Evsegneev <pevsegneev@quotix.com>
2016-05-12 11:05:36 +0300

    ...And more playing with json response

--- telegramstacksearch.go
+++ telegramstacksearch.go
@@ -94,7 +94,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 					}
 
 					w.Header().Set("Content-Type", "application/json")
-					w.Write(b)
+					fmt.Fprintf(w, string(b))
 
 					fmt.Println(b)
 
